29752d82b1993bd92b8c76350d281f9ef970a103
ValeriaDiaz <valeriadiazbel@gmail.com>
2026-07-07 22:39:01 -0500

    hola

--- compararPokemon.js
+++ compararPokemon.js
@@ -1,6 +1,43 @@
-async function consultarPokemon(){
-    const pokemon = prompt ("ingrese su pokemon:");
-    const respuesta = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+async function consultarPokemon(nombrePokemon){
+    const respuesta = await fetch(`https://pokeapi.co/api/v2/pokemon/${nombrePokemon}`);
     const datos = await respuesta.json();
+    return datos
+}
+
+
+
+function obtenerStat (datos, nombreStat){
+
+    for(let kstats = 0; kstats< datos.stats.length; kstats ++){
+        console.log ( "stat.nombre:"  +  datos.stats[kstats].stat.name )
+        if (nombreStat == datos.stats[kstats].stat.name ) {
+           
+            return datos.stats[Kstats].base_stat
+        } 
+    } 
+    return null
+   
+}
+
+function compararPokemon (nombre1, nombre2, stat) {
+
+ let datos1 = consultarPokemon (nombre1) 
+ let base1 = obtenerStat (datos1, stat)
+
+ let datos2 = consultarPokemon (nombre2)
+ let base2 = obtenerStat (datos2, stat)
+
+ if (base1 == null ||base2) { 
+ console.log (" incorrecto")
+ }
+ if (base1 < base2) {
+    console.log ("Gana",nombre2 )
+
+ } else if (base1> base2){
+    console.log ("Gana", nombre1)
+
+ } else { console.log ( "empate")}
+
 
 }
+
